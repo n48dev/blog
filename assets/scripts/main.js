3b34ddb4b190c6dedafee3e15c0177375704f05c
Anthony Buitrago <n48dev@Anthonys-MacBook-Air.local>
2024-11-04 21:40:37 -0500

    Added support for multiple languages (en, es, de)
    Implemented support for multiple languages in the blog settings menu by
    creating separate  HTML files for each language to improve SEO. A JavaScript
    function was added to detect the user's device language and redirect them to
    the appropriate HTML file if no language has been set in the settings.
    Additionally a new root index.html was created to handle redirection from
    "n48dev.github.io/blog".

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
+function configMenu() {
     const configButton = document.getElementById('configButton');
     const configMenu = document.getElementById('configMenu');
-
-    const themeAutomatic = document.getElementById('themeAutomatic');
-    const themeLight = document.getElementById('themeLight');
-    const themeDark = document.getElementById('themeDark');
-
-    const container = document.getElementById('container');
-    const mainTitle = document.getElementById('mainTitle');
-    const divider = document.querySelectorAll('.divider');
-    const eachMainArticles = document.querySelectorAll('.eachMainArticles');
-    const blogArticles = document.querySelectorAll('.blogArticles');
-    const contactInfoP = document.getElementById('contactInfoP');
-    const contactInfoLink = document.querySelectorAll('.contactInfoLink');
     
-    /* Despliegue del botón de configuración */
+    /* ConfigButton show */
     configButton.addEventListener('click', () => {
         configMenu.classList.toggle('show');
     });
@@ -24,8 +12,20 @@ document.addEventListener('DOMContentLoaded', () => {
             configMenu.classList.remove('show');
         }
     });
+
+    /* Handle light/dark/automatic theme */
+    const themeAutomatic = document.getElementById('themeAutomatic');
+    const themeLight = document.getElementById('themeLight');
+    const themeDark = document.getElementById('themeDark');
     
-    /* Cambios de tema claro/oscuro/automático */
+    const container = document.getElementById('container');
+    const mainTitle = document.getElementById('mainTitle');
+    const divider = document.querySelectorAll('.divider');
+    const eachMainArticles = document.querySelectorAll('.eachMainArticles');
+    const blogArticles = document.querySelectorAll('.blogArticles');
+    const contactInfoP = document.getElementById('contactInfoP');
+    const contactInfoLink = document.querySelectorAll('.contactInfoLink');
+
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'light') {
         container.classList.remove('dark-mode');
@@ -191,7 +191,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         localStorage.setItem('theme', 'dark');
     });
-    
+
+    /* Get automatic theme */
     const mediaQueryTheme = window.matchMedia("(prefers-color-scheme: dark)");
     mediaQueryTheme.addEventListener('change', () => {
         if (!localStorage.getItem('theme')) {
@@ -276,4 +277,72 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     };
-});
+
+    /* Language settings  */
+    const langEN = document.getElementById('langEN');
+    const langES = document.getElementById('langES');
+    const langDE = document.getElementById('langDE');
+    const savedLang = localStorage.getItem('lang');
+
+    const userLang = navigator.language || navigator.userLanguage;
+    const currentPage = window.location.pathname.split("/").pop();
+    const completeCurrentPage = window.location.pathname.split("/");
+
+    langEN.addEventListener('click', () => {
+        localStorage.setItem('lang', 'en');
+    })
+    langES.addEventListener('click', () => {
+        localStorage.setItem('lang', 'es');
+    })
+    langDE.addEventListener('click', () => {
+        localStorage.setItem('lang', 'de');
+    })
+    
+    if (savedLang === 'en') {
+        if (currentPage === "index.html" && !completeCurrentPage.includes('en')) {
+            window.location.href = `/blog/en/index.html`;
+        } else if (currentPage === "articles.html" && !completeCurrentPage.includes('en')) {
+            window.location.href = `/blog/en/articles.html`;
+        } else if (currentPage === "aboutme.html" && !completeCurrentPage.includes('en')) {
+            window.location.href = `/blog/en/aboutme.html`;
+        }
+    } else if (savedLang === 'es') {
+        if (currentPage === "index.html" && !completeCurrentPage.includes('es')) {
+            window.location.href = `/blog/es/index.html`;
+        } else if (currentPage === "articles.html" && !completeCurrentPage.includes('es')) {
+            window.location.href = `/blog/es/articles.html`;
+        } else if (currentPage === "aboutme.html" && !completeCurrentPage.includes('es')) {
+            window.location.href = `/blog/es/aboutme.html`;
+        }
+    } else if (savedLang === 'de') {
+        if (currentPage === "index.html" && !completeCurrentPage.includes('de')) {
+            window.location.href = `/blog/de/index.html`;
+        } else if (currentPage === "articles.html" && !completeCurrentPage.includes('de')) {
+            window.location.href = `/blog/de/articles.html`;
+        } else if (currentPage === "aboutme.html" && !completeCurrentPage.includes('de')) {
+            window.location.href = `/blog/de/aboutme.html`;
+        }
+    } else {
+        let langDirectory;
+        if (userLang.startsWith("es")) {
+            langDirectory = "/es/";
+            localStorage.setItem('lang', 'es');
+        } else if (userLang.startsWith("de")) {
+            langDirectory = "/de/";
+            localStorage.setItem('lang', 'de');
+        } else {
+            langDirectory = "/en/";
+            localStorage.setItem('lang', 'en');
+        }
+
+        if (currentPage === "index.html") {
+            window.location.href = `/blog${langDirectory}index.html`;
+        } else if (currentPage === "articles.html") {
+            window.location.href = `/blog${langDirectory}articles.html`;
+        } else if (currentPage === "aboutme.html") {
+            window.location.href = `/blog${langDirectory}aboutme.html`;
+        }
+    }
+};
+
+document.addEventListener("DOMContentLoaded", configMenu);
